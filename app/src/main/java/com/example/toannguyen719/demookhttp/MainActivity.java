b97401eadcbf7805eecefa537a372eb719cf1ef4
toannguyenvan719 <toan.nguyenvan719@gmail.com>
2015-09-13 12:51:19 +0700

    post & get method

--- app/src/main/java/com/example/toannguyen719/demookhttp/MainActivity.java
+++ app/src/main/java/com/example/toannguyen719/demookhttp/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
@@ -17,6 +19,8 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String URL_IMAGE = "http://54.255.214.47/api/product/getimage";
+    public static final String URL_LOGIN = "http://54.255.214.47/users/login";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private Button btnGetMethod;
     private Button btnPostMethod;
@@ -45,7 +49,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btnPostMethod:
+                String json = "{ \n" +
+                        "    \"username\":\"phuoc\",\n" +
+                        "    \"password\":\"12345\"\n" +
+                        "}";
 
+                PostOkHttp postOkHttp = new PostOkHttp();
+                postOkHttp.execute(URL_LOGIN, json);
                 break;
         }
     }
@@ -86,4 +96,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private class PostOkHttp extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String url = params[0];
+            String json = params[1];
+
+            try {
+
+                return PostMethod(url, json);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        private String PostMethod(String url, String json) throws IOException {
+
+            String result;
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = RequestBody.create(JSON, json);
+            Request request = new Request.Builder().url(url).post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            result = response.body().string();
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            tvResult.setText(s);
+        }
+    }
 }
